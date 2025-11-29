e6a4ff4473daef6bfa4aa64350a9aa9367d47fc4
Manuel17ms <manuel.manea@studenti.unitn.it>
2025-11-29 18:45:28 +0100

    Update index.js

---
+++ frontend/src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import ResultView from '../views/ResultView.vue';
+
+const routes = [
+  { path: '/', name: 'home', component: HomeView },
+  { path: '/result', name: 'result', component: ResultView }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+export default router;
