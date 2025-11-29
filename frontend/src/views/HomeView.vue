7d7f435c34141f4bb63461e39eb03ad54119ab78
Manuel17ms <manuel.manea@studenti.unitn.it>
2025-11-29 18:46:20 +0100

    Update HomeView.vue

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <SimulationForm @submitted="onSubmitted" />
+  </div>
+</template>
+
+<script>
+import SimulationForm from '../components/SimulationForm.vue';
+import { useSimulationStore } from '../store/simulationStore';
+
+export default {
+  name: 'HomeView',
+  components: { SimulationForm },
+  setup() {
+    const store = useSimulationStore();
+    store.loadLocations();
+    const onSubmitted = () => {
+      // navigate to results after submission
+      // store.submitSimulation already called by the form; we move to /result
+      // use router push
+      window.location.href = '/result';
+    };
+    return { onSubmitted };
+  }
+};
+</script>
