01b4d65427c9ee416bd5976013dae9a30b3a114d
Manuel17ms <manuel.manea@studenti.unitn.it>
2025-12-08 10:55:23 +0100

    Update simulationStore.js

--- frontend/src/store/simulationStore.js
+++ frontend/src/store/simulationStore.js
@@ -1,23 +1,29 @@
 import { defineStore } from 'pinia';
-import { postSimulation, getLocations } from '../api/simulation';
+import { postSimulation, getLocations, getCompare } from '../api/simulation';
 
 export const useSimulationStore = defineStore('simulation', {
   state: () => ({
-  form: {
-    squareMeters: 80,
-    housingType: 'apartment',
-    residents: 1,
-    energy: { water: 'electricity', heating: 'gas', cooking: 'electricity' },
-    locationId: ''
-  },
-  locations: [],
-  result: null,
-  history: [], 
-  loading: false,
-  compareResult: null,
-  error: null
-  
-}),
+    form: {
+      squareMeters: 80,
+      housingType: 'apartment',
+      residents: 1,
+      energy: {
+        water: 'electricity',
+        heating: 'gas',
+        cooking: 'electricity'
+      },
+      locationId: ''
+    },
+
+    locations: [],
+    result: null,
+    history: [],          
+    compareResult: null,  
+
+    loading: false,
+    error: null
+  }),
+
   actions: {
     async loadLocations() {
       this.loading = true;
@@ -29,34 +35,43 @@ export const useSimulationStore = defineStore('simulation', {
         this.loading = false;
       }
     },
-   async submitSimulation() {
-  this.loading = true;
-  this.error = null;
 
-  try {
-    const res = await postSimulation(this.form);
-    this.result = res.data;
+    async submitSimulation() {
+      this.loading = true;
+      this.error = null;
 
-    this.history.push({
-      ...res.data,
-      date: new Date().toLocaleString()
-    });
+      try {
+        const res = await postSimulation(this.form);
 
-  } catch (err) {
-    this.error = err.message || 'Errore chiamata API';
-  } finally {
-    this.loading = false;
-  }
-}
-async compareLocation(locationId) {
-  try {
-    this.compareResult = await getCompare(locationId);
-  } catch (err) {
-    this.error = 'Errore confronto località';
-  }
-}
+        console.log('RISPOSTA API:', res);
+
+       
+        this.result = res.data;
+
+       
+        this.history.push({
+          ...res.data,
+          date: new Date().toLocaleString()
+        });
+
+      } catch (err) {
+        this.error = err.message || 'Errore chiamata API';
+      } finally {
+        this.loading = false;
+      }
+    },
+
+    async loadCompare(locationId) {
+      try {
+        this.compareResult = await getCompare(locationId);
+      } catch (err) {
+        this.error = err.message || 'Errore confronto';
+      }
+    }
   }
 });
+
+
 
 
 
