acb52b4766e7b7fb10746c037f7246155b78e545
Manuel17ms <manuel.manea@studenti.unitn.it>
2026-02-11 22:39:10 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,8 @@
-
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import { connectDB } from './config/db.js'
 
+import { connectDB } from './config/db.js'
 import simulationRoutes from './routes/simulation.routes.js'
 import localitaRoutes from './routes/localita.js'
 
@@ -14,31 +13,24 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-// ROUTES
 app.use('/api', simulationRoutes)
 app.use('/api', localitaRoutes)
 
-// ROOT
-app.get('/', (req, res) => {
-  res.json({ message: 'RF4 Simulation Backend' })
-})
+connectDB(process.env.MONGODB_URI)
 
-// CONNECT DB
-const uri = process.env.MONGODB_URI
-connectDB(uri)
-
-// ⭐ esporta app per Jest
+// export per test
 export default app
 
-// ⭐ avvia server SOLO se eseguito direttamente
 if (process.env.NODE_ENV !== 'test') {
 
   const PORT = process.env.PORT || 4000
 
   app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+    console.log('Server running')
   })
+
 }
+
 
 
 
