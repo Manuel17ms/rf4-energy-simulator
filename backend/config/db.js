9a8323c6d86c084c5a2e9fdc055e4127b848dd25
Manuel17ms <manuel.manea@studenti.unitn.it>
2026-02-11 22:44:30 +0100

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,25 @@
-
 import mongoose from 'mongoose'
 
 export async function connectDB(uri) {
 
   try {
+
     await mongoose.connect(uri)
     console.log('MongoDB connected')
+
   } catch (error) {
+
     console.error(error)
-    process.exit(1)
+
+    // 🔥 NON uscire nei test
+    if (process.env.NODE_ENV !== 'test') {
+      process.exit(1)
+    }
+
+    throw error
   }
 
 }
+
+
+
